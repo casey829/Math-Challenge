307e7b717370d197e50c27efe41be6b6818938c1
casey829 <caseymwangi31@gmail.com>
2025-01-29 17:59:10 +0300

    adding logic

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import random
+import time
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 2
 MAX_OPERAND = 15
+TOTAL_PROBLEMS = 10
 
 
 def generate_problem():
@@ -12,7 +14,27 @@ def generate_problem():
     
     
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem()
+
+wrong = 0
+input("Press enter to start!")
+print('____________')
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Question #" + str(i + 1) + ": " + expr + ' = ')
+        if guess == str(answer):
+            break
+        wrong += 1
+        
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+        
+print("_________")
+print("Good job you finished in", total_time, "seconds!" )
+
